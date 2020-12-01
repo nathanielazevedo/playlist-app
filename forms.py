848110151e8ce6154e2140de78b06287b602df65
nathanielazevedo <nathanielazevedo@hotmail.com>
2020-11-30 21:19:34 -0800

    forms

---
+++ forms.py
@@ -0,0 +1,38 @@
+"""Forms for playlist app."""
+
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
+from flask_wtf import FlaskForm
+
+
+class PlaylistForm(FlaskForm):
+    """Form for adding playlists."""
+
+    name = StringField(
+        "Name",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    description = StringField(
+        "Description",
+        validators=[InputRequired()],
+    )
+
+
+class SongForm(FlaskForm):
+    """Form for adding songs."""
+
+    title = StringField(
+        "title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    artist = StringField(
+        "artist",
+        validators=[InputRequired()],
+    )
+
+
+# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
+class NewSongForPlaylistForm(FlaskForm):
+    """Form for adding a song to playlist."""
+
+    song = SelectField('Song To Add', coerce=int)
